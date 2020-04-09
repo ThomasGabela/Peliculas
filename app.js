1969e3518ed6f45f367d07fe2e05bd591be639a4
ThomasGabela <thomasgabela@hotmail.com>
2020-04-09 01:27:14 -0300

    Peliculas casi

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require ("http")
+const Movies = require("./movies")
+const Faq = require("./faq")
+const Sucursales = require("./sucursales")
+const Home = require("./home")
+const Contacto = require("./contacto")
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type' : "text/play; charset = utf-8"})
+    let respuesta = "";
+    console.log(req.url)
+    switch (req.url) {
+        case '/' : respuesta = Home; break;
+        case '/en-cartelera' : respuesta = Movies ; break;
+        case '/sucursales' : respuesta = Sucursales; break;
+        case 'contacto' : respuesta = Contacto; break;
+        case 'preguntas frecuentes' : respuesta = Faq; break;
+        default: respuesta = "Error";
+
+    }
+    res.end(respuesta)
+}).listen(300, "localhost")
+
