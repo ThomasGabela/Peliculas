5cc5060dbd1566a786715a4bb61ae7cf22744c27
ignaciospc <ignacio.n.soria@hotmail.com>
2020-04-09 18:29:52 -0300

    se optimizo en un solo forEach la lunea final de respuesta

--- cartelera.js
+++ cartelera.js
@@ -11,13 +11,18 @@ let respuesta = "En Cartelera";
 
 respuesta += "\nExiste un total de : " + cantidad;
 
+js.forEach(elemento => respuesta += "\n\n\ntitulo: " + elemento.title + "\nreseña: " + elemento.overview + "\n---------------------------------------------------------------------------------\n") 
+ 
 
-js.forEach(elemento => name.push(elemento.title));
+/*js.forEach(elemento => name.push(elemento.title));
 js.forEach(elemento => reseña.push(elemento.overview));
 
 for (i = 0; i < name.length; i++){
     respuesta += "\n\n\n" + name[i] + "\n" + reseña[i] + "\n----------------------------------------------------------------\n"
- }
+ }*/
+
 
  
  module.exports = respuesta;
+
+
